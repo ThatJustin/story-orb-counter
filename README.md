98459a0a0d56f3e259a284d2e58c681a76c3c86e
ThatJustin <62494960+ThatJustin@users.noreply.github.com>
2021-09-02 18:52:28 -0400

    added parsing pages support, updated README

--- README.md
+++ README.md
@@ -1,6 +1,23 @@
 # story-orb-counter
 
-Gets the total and individual spirit orb count from stories. Data is parsed from https://bbs-simulator.com/story.
+Gets the total and individual spirit orb count from stories (main story, side story, TYBW story, sub story, event, chronicles). Data is parsed from https://bbs-simulator.com/story.
+
+## Orb Count
+If you just want to see the orb count instead of running it:
+Last Updated (MM/DD/YYYY): **9/2/2021**
+
+Main Story Total Orb Count:  **3110** 
+Side Story Total Orb Count:  **5248** 
+TYBW Story Total Orb Count:  **586**
+Sub Story Total Orb Count:  **2612**
+Event Total Orb Count:  **25278**
+Chronicles' Total Orb Count:  	**467**
+
+## Setup
+1) Download or clone repository. 
+2) Download [chromedriver](https://chromedriver.chromium.org/downloads).
+3) Place it in the project directory or wherever you prefer. (By default it  looks for for chromedriver.exe in story-orb-counter/chromedriver/) 
+	* If you place chromedriver.exe anywhere else, please modify `main.py` line 9 for your location.
 
 ## Usage
 * Run main.py normally through command line.
@@ -12,12 +29,8 @@ Gets the total and individual spirit orb count from stories. Data is parsed from
   ```
 
 ## Dependencies
-* BeautifulSoup4  4.9.3
+* BeautifulSoup 4  4.9.3
 * Selenium 3.141.0
-* webdriver-manager 3.3.0
-## Roadmap
-
-* Use selenium to simulate clicking to allow more stories to load.
 
 ## Credits
 Big thank you to [Souldex](https://www.patreon.com/Souldex) for creating the [BBS Simulator](https://bbs-simulator.com/) website.
--- main.py
+++ main.py
@@ -1,23 +1,22 @@
 from bs4 import BeautifulSoup, Tag
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
-url = "https://bbs-simulator.com/story"
+url = 'https://bbs-simulator.com/story'
+driver = webdriver.Chrome('./chromedriver/chromedriver.exe')
 driver.get(url)
 delay = 2  # in seconds
 try:
-    myElem = WebDriverWait(driver, delay).until(expected_conditions.presence_of_element_located((By.ID, 'storyTabSearch5')))
-    print("Page is ready!")
+    myElem = WebDriverWait(driver, delay).until(
+        expected_conditions.presence_of_element_located((By.ID, 'storyTabSearch5')))
+    print("Page loaded successfully.")
 except TimeoutException:
-    print("Loading took too much time!")
+    print(f"Page loading took too long.")
 
-source = driver.page_source
-soup = BeautifulSoup(source, "html.parser")
+soup = BeautifulSoup(driver.page_source, "html.parser")
 
 orb_information = dict()
 
@@ -26,30 +25,32 @@ def is_invalid_name(name):
     return name == 'Rewards' or name == 'Drops' or name == 'Bonus Units' or name == 'Requires'
 
 
-def parse_story_name(information_div, category_name):
+def parse_story_name(card_body, category_name, card_length):
     """
-    Parses the story name from the given information_div and assigns it to a dictionary with the category_name.
-    :param information_div: div where the story name is held
+    Parses the story name from the given card_body and assigns it to a dictionary with the category name.
+    :param card_body: div where the story name is held
     :param category_name: category of the story
+    :param card_length: length of cards to parse
     :return: None
     """
-    index = 0
-    for name in information_div.findChildren('b'):
-        name_text = name.text.replace('<b>', '').replace('</b>', '')
+    index = card_length if (card_length > 0) else 0
+    for name in card_body.findChildren('b'):
+        name_text: str = name.text.replace('<b>', '').replace('</b>', '')
         if not is_invalid_name(name_text) and not None:
-            orb_information[category_name][index] = [name_text, -1]
+            orb_information[category_name][index] = [name_text, 0]
             index = index + 1
 
 
-def parse_orb_count(information_div, category_name):
+def parse_orb_count(card_body, category_name, card_length):
     """
-    Parses the orb count from the information_div by searching for the image of it.
-    :param information_div: div where the story name is held
+    Parses the orb count from the card_body by searching for the image of it.
+    :param card_body: div where the story name is held
     :param category_name: category of the story
+    :param card_length: length of cards to parse
     :return: None
     """
-    index = 0
-    for orb_info in information_div.find_all("img", {
+    index = card_length if (card_length > 0) else 0
+    for orb_info in card_body.find_all("img", {
         "src": "https://bucket.bbs-simulator.com/item/0017-ce1df5d4-c569-4794-b260-3a39d5e13891.png"}):
         if orb_info is not None:
             orb_count = int(orb_info.parent.span.text)
@@ -67,33 +68,69 @@ def parse_category_name(category_div):
     if name_holder is None or name_holder.has_attr('aria-label') is None:
         return "Error. Name not found."
 
-    cat_name = (str(name_holder.attrs['aria-label']).replace(" name", ""))
-    orb_information[cat_name] = {}
-    return cat_name
+    category_name = (str(name_holder.attrs['aria-label']).replace(" name", ""))
+    orb_information[category_name] = {}
+    return category_name
 
 
-def parse_website():
+def change_story_tab(tab_index):
     """
-    Parses the website for story and orb information.
+    Changes the page focus to a new story tab based on the input index and updates the soup object.
+    :param tab_index: story tab index
+    :return: None
+    """
+    global soup
+    driver.find_element_by_xpath(f"//a[@data-rb-event-key='storyTab{tab_index}']").click()
+
+    # Update the soup object so Selenium can see the new elements
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+
+
+def get_tab_panel(tab_index):
+    tab_content_div = soup.find("div", {"class": "tab-content"})
+    tab_content_div: Tag
+    for index, tab_panel_div in enumerate(tab_content_div):
+        if index == tab_index:
+            return tab_panel_div
+    return None
+
+
+def get_card_length(card_body):
+    for child in card_body.findChildren():
+        if "Showing 1-" in child.text:
+            return int(child.text.split()[-1])
+    return 0
+
+
+def get_story_tab_count():
+    return len(soup.find("nav", {"class": "nav-tabs-dark nav nav-tabs nav-fill"}))
+
+
+def parse_story():
+    """
+    Parses the website for story orb information.
     :return: None
     """
     print(f"Parsing {url} for orb information\n")
-    tab_content_div = soup.find("div", {"class": "tab-content"})
-    tab_content_div: Tag
-    for tab_panel_div in tab_content_div:
+
+    story_tab_count = get_story_tab_count()
+
+    for index in range(0, story_tab_count):
+        tab_panel_div = get_tab_panel(index)
+        if tab_panel_div is None:
+            return
         category_div = tab_panel_div.find("div", {"class": "row"})
-        category_name = parse_category_name(category_div)  # name of the category
+        category_name = parse_category_name(category_div)
 
-        # div where story name and orb count is located
-        information_div = tab_panel_div.find("div", {"class": "w-100 card-body"})
+        card_body = tab_panel_div.find("div", {"class": "w-100 card-body"})
+        total_card_length = get_card_length(card_body)
 
-        parse_story_name(information_div, category_name)  # story name
-        parse_orb_count(information_div, category_name)  # orb count
+        parse_tab(index, category_name, card_body, total_card_length)
 
 
 def print_information():
     """
-    Prints out orb_information dictionary for easier readability.
+    Prints out orb_information dictionary.
     :return: None
     """
     story_orb_totals_lst = [0] * 6
@@ -101,20 +138,50 @@ def print_information():
     for category_type_key, category_type_value in orb_information.items():
         print(category_type_key, '\n')
         for story_key, orb_info_key in category_type_value.items():
-            story_name = orb_info_key[0]
+            story_name = orb_info_key[0].encode('ascii', 'ignore').decode('ascii')
             orb_count = orb_info_key[1]
-            print(story_name, "'s Orb Count: ", orb_count)
+            print(story_name, "Orb Count: ", orb_count)
             story_orb_totals_lst[index] = story_orb_totals_lst[index] + orb_count
         print()
-        print(category_type_key, "'s Total Orb Count: ", story_orb_totals_lst[index], '\n\n')
+        print(category_type_key, "Total Orb Count: ", story_orb_totals_lst[index], '\n\n')
         index = index + 1
 
     print("Total Orb Count: ", sum(story_orb_totals_lst))
 
 
+def click_next_page(btn_index):
+    global soup
+    next_btn = driver.find_elements_by_xpath('//*[text() = ">"]')[btn_index * 2]
+    next_btn.click()
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+
+
+def parse_tab(index, category_name, card_body, total_card_length, new_card_index=0):
+    """
+      Parses the visible tab, a bunch of cards in the card body.
+      :return: None
+      """
+    if total_card_length == new_card_index:
+        return
+    if index > 0:
+        change_story_tab(index)
+    visible_card_length = len(card_body.find_all("div", {"class": "card"}, recursive=False))
+
+    parse_story_name(card_body, category_name, new_card_index)
+    parse_orb_count(card_body, category_name, new_card_index)
+
+    click_next_page(index)
+
+    tab_panel_div = get_tab_panel(index)
+    card_body = tab_panel_div.find("div", {"class": "w-100 card-body"})
+
+    parse_tab(index, category_name, card_body, total_card_length,
+              new_card_index + visible_card_length)
+
+
 def main():
     try:
-        parse_website()
+        parse_story()
         print_information()
     finally:
         driver.quit()
